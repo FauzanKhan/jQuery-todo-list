4e1854f911530e820a31c76cd5ac5951a02fbe97
FauzanKhan <fauzankhan93@gmail.com>
2016-04-16 11:48:58 +0530

    Set up project structure & did HTML+CSS work

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var notify = require('gulp-notify');
+var rename = require("gulp-rename");
+
+var path = {
+	src : {
+		scss: 'src/scss/app.scss',
+		js: 'src/js/app.js'
+	},
+	allScss: 'src/scss/*.scss',
+	dest: {
+		css: 'public/css',
+		js: 'public/js'
+	}
+}
+
+gulp.task('sass', function(){
+	gulp.src(path.src.scss)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(path.dest.css))
+		.pipe(notify({
+	      message: "Generated <%= file.relative %>",
+	    }))
+});
+
+gulp.task('minify-js', function(){
+	gulp.src(path.src.js)
+		.pipe(uglify())
+		.pipe(rename({'suffix': '.min'}))
+		.pipe(gulp.dest(path.dest.js))
+		.pipe(notify({
+	      message: "Generated <%= file.relative %>",
+	    }));
+});
+
+gulp.task('minify', ['sass', 'minify-js']);
+
+gulp.task('watch', function(){
+	gulp.watch(path.allScss, ['sass']);
+	gulp.watch(path.src.js, ['minify-js']);
+})
